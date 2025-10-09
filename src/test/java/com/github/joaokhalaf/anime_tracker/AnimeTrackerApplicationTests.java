c9496a2cdbe81308c6df9e8a7f58d61e6de0568f
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-10-08 22:28:40 -0300

    first commit

---
+++ src/test/java/com/github/joaokhalaf/anime_tracker/AnimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joaokhalaf.anime_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
