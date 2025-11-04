43e7612099f2c9d2d5f1f67d3c30933f8669e1d9
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-11-03 23:54:58 -0300

    fix: read .env files

--- src/main/java/com/github/joaokhalaf/anime_tracker/AnimeTrackerApplication.java
+++ src/main/java/com/github/joaokhalaf/anime_tracker/AnimeTrackerApplication.java
@@ -2,12 +2,22 @@ package com.github.joaokhalaf.anime_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AnimeTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory(".")
+                .ignoreIfMissing()
+                .ignoreIfMalformed()
+                .load();
 
+        dotenv.entries().forEach(e ->
+            System.setProperty(e.getKey(), System.getProperty(e.getKey(), e.getValue()))
+        );
+
+        SpringApplication.run(AnimeTrackerApplication.class, args);
+    }
 }
